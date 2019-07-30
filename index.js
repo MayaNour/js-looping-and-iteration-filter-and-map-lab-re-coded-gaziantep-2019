d6ade838d26d4b07664e80dc32747d0736edeea5
Maya Nour Alah <maya.noorallah.89@gmail.com>
2019-07-30 15:53:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution here:
 function driversWithRevenueOver(drivers, revenueValue){
   const newDriversFilterList = drivers.filter({
-    function (driver.name) { return driver.revenue > revenueValue; }
+    function (driver) { return driver.revenue > revenueValue; }
   });
   
   return newDriversFilterList;
