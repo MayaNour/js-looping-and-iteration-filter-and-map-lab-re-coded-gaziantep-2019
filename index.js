983b27acc351fa141b5c9d73925ae46bded7915e
Maya Nour Alah <maya.noorallah.89@gmail.com>
2019-07-30 16:15:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,7 @@
 function driversWithRevenueOver(drivers, revenueValue){
     const newDriversFilterList = drivers.filter(
       function (driver) { return driver.revenue > revenueValue; }
-    ).map(function(driver){
-      return driver.name;
-    });
+    );
     
     return newDriversFilterList;
   }
