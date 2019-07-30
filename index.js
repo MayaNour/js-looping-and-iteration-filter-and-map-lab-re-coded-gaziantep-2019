dc4f39d6e9dd9c9d634b40f9d4cb864c0f4c31d4
Maya Nour Alah <maya.noorallah.89@gmail.com>
2019-07-30 15:43:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution here:
 function driversWithRevenueOver(drivers, revenueValue){
   const newDriversFilterList = drivers.filter({
-    
+    function (driver) { return driver > revenueValue; }
   });
 }
